e2ab0ce5335017403f2a1cd9b5f7e12eef0a6d77
Caroline Moran <carolvandrade@gmail.com>
2023-12-03 08:47:54 -0400

    feat: ajuste

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./global.scss";
+import ReactDOM from "react-dom";
 
 import { Home } from "./pages/Home";
+import "./global.scss";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Home />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
